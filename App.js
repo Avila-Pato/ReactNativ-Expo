5564459fed3b07f52f53aff1c4c96a336bff4c31
Patricio Avila <p.avil;af1998@gmail.com>
2024-12-05 20:10:20 -0300

    Setting upthe maps and marker

--- App.js
+++ App.js
@@ -1,34 +1,65 @@
 import React from "react";
-import { Text, View, StyleSheet,Image } from "react-native";
-import image from './assets/texting.jpg'
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker, Polyline } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
 
-const App = () => {
+import { GOOGLE_MAPS_KEY } from '@env';
+
+export default function App() {
+  const [origin, setOrigin] = React.useState({
+    latitude: -33.4489, // Santiago de Chile
+    longitude: -70.6693,
+  });
+
+  const [destination, setDestination] = React.useState({
+    latitude: -33.6111, // Puente Alto
+    longitude: -70.5750,
+  });
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello world</Text>
-      <Image source={{uri: 'https://picsum.photos/200/300'}}
-      style={styles.image} >
-      </Image>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: (origin.latitude + destination.latitude) / 2,
+          longitude: (origin.longitude + destination.longitude) / 2,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+      >
+        {/* Marcadores */}
+        <Marker
+          draggable={true}
+          onDragEnd={(direction) => setDestination(direction.nativeEvent.coordinate)}
+          coordinate={destination}
+        />
+        <Marker
+          draggable={true}
+          onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)}
+          coordinate={origin}
+        />
 
-      <Image 
-      source={image}
-      style={styles.image}>
-      </Image>
+        {/* Línea entre dos puntos */}
+        <Polyline coordinates={[origin, destination]} strokeColor="#2fef" strokeWidth={3} />
 
+        {/* Mapa con la dirección */}
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_KEY}
+        />
+      </MapView>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#292929",
+    backgroundColor: "#fff",
   },
-  title: { fontSize: 30, color: "#fff" },
-  image : {height: 200, width: 200},
- },
-);
-
-export default App
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
